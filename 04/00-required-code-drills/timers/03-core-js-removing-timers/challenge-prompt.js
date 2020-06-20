ab1aa881c6ee7e3bc8157fe5db5650d453736834
Bart Dority <b0rgBart3@gmail.com>
2020-06-19 21:14:20 -0700

    drill 04/03 completed

--- 04/00-required-code-drills/timers/03-core-js-removing-timers/challenge-prompt.js
+++ 04/00-required-code-drills/timers/03-core-js-removing-timers/challenge-prompt.js
@@ -17,41 +17,50 @@ var three = setTimeout(log("Question1-3"), 1000 * 7);
 
 // remove the timers `one`, `two`, and `three`
 
-
+clearTimeout(one);
+clearTimeout(two);
+clearTimeout(three);
 
 console.log("==================== Question 02 ====================");
 // create a timer that logs "Hello" after 17 seconds
-
+var four = setTimeout(log("Hello"), 1000 * 17);
 
 
 // remove the timer you just made
 
-
+clearTimeout(four);
 
 console.log("==================== Question 03 ====================");
 // create a timer that logs "Goodbye" after 17 seconds
-
+var five = setTimeout(log("Goodbye"), 1000 * 17);
 
 
 // create a timer that removes the "Goodbye" timer after 5 seconds
 // HINT: much like the `log` function we defined at the top of the page,
 // you're going to have to create a function that returns another function
 
-
+setTimeout( clearTimeout(five), 1000 * 5);
 
 console.log("==================== Question 04 ====================");
 // create a timer that logs "Part 1" after 3 seconds
-
+var six = setTimeout(log("Part 1"), 1000 * 3);
 
 
 // create a timer that removes the "Part 1" timer after 2 seconds and also
 // console logs out "Part 2" when it runs
 
-
+var seven = setTimeout(function() {
+  clearTimeout(six); 
+  log("Part 2");
+}, 1000 * 2);
 
 // create a timer that removes the "Part 2" timer after 1 second and also
 // console logs out "Part 3" when it runs
 
+var seven = setTimeout(function() {
+  clearTimeout(seven); 
+  log("Part 3");
+}, 1000 * 1);
 
 
 console.log("==================== Question 05 ====================");
@@ -71,4 +80,6 @@ var alertArray = [
 
 // remove all the timers created by `alertArray`.
 
-
+alertArray.forEach(function(timer) {
+  clearTimeout(timer);
+});
